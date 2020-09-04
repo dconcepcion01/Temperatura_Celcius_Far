49b904e113722231777cffc4dd9f076a82a7bd8a
Danny <dannyconcepcion01@gmail.com>
2020-09-04 19:57:05 -0400

    Agregar archivos de proyecto.

---
+++ Temperatura_Celcius_Far/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatura_Celcius_Far
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Covertir de grados celsius a faren
+
+            double celsius, fahrenheit;
+
+            Console.WriteLine("Digite la temperatura Celsius a Convertir en Fahrenheit");
+            celsius = double.Parse(Console.ReadLine());
+
+            fahrenheit = (celsius=  90  * 9 / 5) + 32;
+            fahrenheit = Convert.ToInt32(fahrenheit);
+
+            Console.WriteLine("El equivalente a fahrenheit es:  " + fahrenheit);
+
+            Console.ReadKey();
+        }
+    }
+}
